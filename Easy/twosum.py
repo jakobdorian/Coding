893e9f9b0c0a85f9ea84d3b7dbfe6816aa4675dd
jakobdorian <79760777+jakobdorian@users.noreply.github.com>
2023-10-15 11:31:04 +0800

    added sol for two sum problem

---
+++ Easy/twosum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        found = False
+        i = 0
+        res = []
+        # iterate through the list looking at two items at a time
+        while i < len(nums) and not found:
+            j = i + 1
+            while j < len(nums) and not found:
+                # if the two items equal target then it is found
+                if nums[i] + nums[j] == target:
+                    found = True
+                    res.append(i)
+                    res.append(j)
+                j += 1
+            i += 1
+        return res
