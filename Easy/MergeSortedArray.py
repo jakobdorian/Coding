f2bd93f82739951b6b764fa004d12d6c061c04d9
jakobdorian <79760777+jakobdorian@users.noreply.github.com>
2023-10-17 11:42:07 +0800

    added sol for merge sorted array

---
+++ Easy/MergeSortedArray.py
@@ -0,0 +1,24 @@
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        last_index = m + n - 1
+
+        # merge list in reverse order
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last_index] = nums1[m - 1]
+                # update pointers
+                m -= 1
+            else:
+                nums1[last_index] = nums2[n - 1]
+                # update pointers
+                n -= 1
+            last_index -= 1
+        # fill nums1 with leftover nums2 elements
+        while n > 0:
+            nums1[last_index] = nums2[n - 1]
+            n = n - 1
+            last_index = last_index - 1
