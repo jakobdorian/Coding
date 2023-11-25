3dfd25ac0684c76983f14762dc9830d8204c1b1c
Jakob <79760777+jakobdorian@users.noreply.github.com>
2023-11-25 11:42:15 +0800

    fixed sol

--- Top Interview 150 in Python/Medium/LongestConsecutiveSequence.py
+++ Top Interview 150 in Python/Medium/LongestConsecutiveSequence.py
@@ -5,11 +5,11 @@ class Solution:
         numSet = set(nums)
         # longest consecutive sequence
         res = 0
-        length = 0
 
         for n in nums:
             # check if its the start of a sequence
             if (n - 1) not in numSet:
+                length = 1
                 while (n + length) in numSet:
                     length += 1
                 res = max(length, res)
